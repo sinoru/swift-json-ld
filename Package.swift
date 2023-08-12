877a2b38540da03d818ac3c75662fb3467833fef
Jaehong Kang <sinoru@me.com>
2023-08-12 14:55:28 +0900

    Renaming Targets

--- Package.swift
+++ Package.swift
@@ -8,16 +8,16 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "swift-json-ld",
-            targets: ["swift-json-ld"]),
+            name: "JSON-LD",
+            targets: ["JSON-LD"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "swift-json-ld"),
+            name: "JSON-LD"),
         .testTarget(
-            name: "swift-json-ldTests",
-            dependencies: ["swift-json-ld"]),
+            name: "JSON-LDTests",
+            dependencies: ["JSON-LD"]),
     ]
 )
